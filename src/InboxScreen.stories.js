805c9d8736bf360739281dfcdaec1a173b36da27
Shota Yamahira <shyamahira@bbtower.co.jp>
2022-06-28 05:31:22 +0900

    test: Interaction: Write an interaction test using the play function

--- src/InboxScreen.stories.js
+++ src/InboxScreen.stories.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { rest } from 'msw';
 import { InboxScreen } from './InboxScreen';
 import { Default as TaskListDefault } from './components/TaskList.stories';
+import { findByRole, userEvent, within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
 
 export default {
   component: InboxScreen,
@@ -33,4 +35,24 @@ Error.parameters = {
       })
     ]
   }
+};
+
+export const PinTask = Template.bind({});
+PinTask.parameters = Default.parameters;
+PinTask.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  const getTask = (name) => canvas.findByRole('listitem', { name });
+
+  // Find the task to pin
+  const itemToPin = await getTask('Export logo');
+
+  // Find the pin button
+  const pinButton = await findByRole(itemToPin, 'button', { name: 'pin' });
+
+  // Click the pin button
+  await userEvent.click(pinButton);
+
+  // Check that the pin button is now a unpin button
+  const unpinButton = within(itemToPin).getByRole('button', { name: 'unpin' });
+  await expect(unpinButton).toBeInTheDocument();
 };
