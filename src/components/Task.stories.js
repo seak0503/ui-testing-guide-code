e7fa5dbbdb776df3cfa8af8906a98ce18c69db40
Shota Yamahira <shyamahira@bbtower.co.jp>
2022-07-02 16:15:10 +0900

    test: Workflow: During development

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -7,7 +7,8 @@ export default {
   argTypes: {
     onArchiveTask: { action: 'onArchiveTask' },
     onTogglePinTask: { action: 'onTogglePinTask' },
-    onEditTitle: { action: 'onEditTitle' }
+    onEditTitle: { action: 'onEditTitle' },
+    onDeleteTask: { action: 'onDeleteTask' }
   }
 };
 
